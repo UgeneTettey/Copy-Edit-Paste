b803cdef702ad0947ba8f49954b0ba3e7fee5815
Eugene <ugeneayerkain@gmail.com>
2024-10-16 21:24:51 +0000

    add dictionary to code

--- ConvertDataType.py
+++ ConvertDataType.py
@@ -1,11 +1,26 @@
 #  Function to convert data types of columns to specified types
 
-"""This function requires additional code where user creates a dictionary of the desired convertions.
-The columns as the keys and corrected datatypes as values."""
+"""create a dictionary of the desired convertions. The columns as the keys and correct datatypes as values.
+The function converts the datatypes of the columns as specified in the dictionary"""
 
 
-def convert_columns_to_datatypes(data,column_datatypes):
+
+# Dictionary to define the changes
+column_datatypes = {
+    'column_name': data_type
+
+}
+
+
+# Function to convert columns to specified data types
+def change_columns_datatypes(data, column_datatypes):
     for column, datatype in column_datatypes.items():
         if column in data.columns:
-            data[column]=data[column].astype(datatype)
+            try:
+                data[column] = data[column].astype(datatype)
+            except ValueError as e:
+                print(f"Error converting column {column}: {e}")
     return data
+
+# Convert the DataFrame
+df = change_columns_datatypes(df, column_datatypes)
